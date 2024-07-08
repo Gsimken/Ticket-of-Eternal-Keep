b0ad5f64cdfcd81ac4c2f11f90d91878d1c07572
동헌희 <hui1601@naver.com>
2024-07-09 04:45:49 +0900

    clean: Change loot strings to constant

--- src/main/java/net/gsimken/event/ModLootTableModifier.java
+++ src/main/java/net/gsimken/event/ModLootTableModifier.java
@@ -6,6 +6,7 @@ import net.gsimken.utils.TicketUtils;
 import net.minecraft.component.ComponentType;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTables;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.entry.LeafEntry;
@@ -22,32 +23,34 @@ public class ModLootTableModifier {
 
     static {
         // Ancient City
-        LOOT_TABLES_WITH_PROBABILITIES.put(Identifier.of("minecraft", "chests/ancient_city"), 0.10f);
+        LOOT_TABLES_WITH_PROBABILITIES.put(LootTables.ANCIENT_CITY_CHEST.getValue(), 0.10f);
 
         // Mineshaft
-        LOOT_TABLES_WITH_PROBABILITIES.put(Identifier.of("minecraft", "chests/abandoned_mineshaft"), 0.03f);
+        LOOT_TABLES_WITH_PROBABILITIES.put(LootTables.ABANDONED_MINESHAFT_CHEST.getValue(), 0.03f);
 
         // Stronghold
-        LOOT_TABLES_WITH_PROBABILITIES.put(Identifier.of("minecraft", "chests/stronghold_library"), 0.05f);
-        LOOT_TABLES_WITH_PROBABILITIES.put(Identifier.of("minecraft", "chests/stronghold_corridor"), 0.05f);
-        LOOT_TABLES_WITH_PROBABILITIES.put(Identifier.of("minecraft", "chests/stronghold_crossing"), 0.05f);
+        LOOT_TABLES_WITH_PROBABILITIES.put(LootTables.STRONGHOLD_LIBRARY_CHEST.getValue(), 0.05f);
+        LOOT_TABLES_WITH_PROBABILITIES.put(LootTables.STRONGHOLD_CORRIDOR_CHEST.getValue(), 0.05f);
+        LOOT_TABLES_WITH_PROBABILITIES.put(LootTables.STRONGHOLD_CROSSING_CHEST.getValue(), 0.05f);
 
         // End Cities
-        LOOT_TABLES_WITH_PROBABILITIES.put(Identifier.of("minecraft", "chests/end_city_treasure"), 0.05f);
+        LOOT_TABLES_WITH_PROBABILITIES.put(LootTables.END_CITY_TREASURE_CHEST.getValue(), 0.05f);
 
         // Bastions
-        LOOT_TABLES_WITH_PROBABILITIES.put(Identifier.of("minecraft", "chests/bastion_bridge"), 0.15f);
-        LOOT_TABLES_WITH_PROBABILITIES.put(Identifier.of("minecraft", "chests/bastion_hoglin_stable"), 0.15f);
-        LOOT_TABLES_WITH_PROBABILITIES.put(Identifier.of("minecraft", "chests/bastion_other"), 0.15f);
-        LOOT_TABLES_WITH_PROBABILITIES.put(Identifier.of("minecraft", "chests/bastion_treasure"), 0.15f);
+        LOOT_TABLES_WITH_PROBABILITIES.put(LootTables.BASTION_BRIDGE_CHEST.getValue(), 0.15f);
+        LOOT_TABLES_WITH_PROBABILITIES.put(LootTables.BASTION_HOGLIN_STABLE_CHEST.getValue(), 0.15f);
+        LOOT_TABLES_WITH_PROBABILITIES.put(LootTables.BASTION_OTHER_CHEST.getValue(), 0.15f);
+        LOOT_TABLES_WITH_PROBABILITIES.put(LootTables.BASTION_TREASURE_CHEST.getValue(), 0.15f);
     }
 
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
-            if (LOOT_TABLES_WITH_PROBABILITIES.containsKey(key.getValue())) {
-                tableBuilder.pool(addTicketToPool(TicketUtils.createTicket(), LOOT_TABLES_WITH_PROBABILITIES.get(key.getValue())));
+            Identifier id = key.getValue();
+            if (LOOT_TABLES_WITH_PROBABILITIES.containsKey(id)) {
+                tableBuilder.pool(addTicketToPool(TicketUtils.createTicket(), LOOT_TABLES_WITH_PROBABILITIES.get(id)));
             }
-            else if(key.getValue().getPath().startsWith("chests/")) {
+            // Add a small chance to all chests
+            else if(id.getPath().startsWith("chests/")) {
                 tableBuilder.pool(addTicketToPool(TicketUtils.createTicket(), 0.005f));
             }
         });
