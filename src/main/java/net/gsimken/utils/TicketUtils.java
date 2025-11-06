5c2e7df1290d964ca2732a600f297997170e3b78
Gary Simken Ruminot <gary.simken@usach.cl>
2025-11-06 13:28:37 -0300

    fix v2

--- src/main/java/net/gsimken/utils/TicketUtils.java
+++ src/main/java/net/gsimken/utils/TicketUtils.java
@@ -35,8 +35,8 @@ public class TicketUtils {
         }
     }
 
-    public static void applyVanishCurse(ServerPlayerEntity player, ServerPlayerEntity oldPlayer) {
-        if(oldPlayer == null || oldPlayer.isInCreativeMode()){
+    public static void applyVanishCurse(ServerPlayerEntity player, boolean isInCreativeMode) {
+        if(isInCreativeMode){
             return;
         }
         String vanishCurse = Enchantments.VANISHING_CURSE.getValue().toString();
